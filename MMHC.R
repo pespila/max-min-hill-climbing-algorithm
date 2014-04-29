a94693f93db63a6a8735948c9976105bf06996ac
Michael <michael@michael-V5-131.(none)>
2014-04-29 18:59:17 +0200

    Worked on the R files.

--- MMHC.R
+++ MMHC.R
@@ -1,5 +1,5 @@
-library("inline")
-library("bnlearn")
+# library("inline")
+# library("bnlearn")
 library("Rcpp")
 sourceCpp("mmpc.cpp");
 
@@ -29,62 +29,166 @@ MMPC.Example <- function(rows,cols=5) {
         return (observedMatrix)
 }
 
-MMPC.Test <- function(rows) {
-        n <- 1
-        MyMatrix <- matrix(,rows,200)
-        for(i in 1:200) {
-                for(j in 1:rows) {
-                        MyMatrix[j,n] <- sample(c(0,1),1,replace=TRUE,prob=c(0.2,0.8))
-                }
-                n <- n + 1
-        }
-        rownames(MyMatrix) <- 1:rows
-        # colnames(MyMatrix) <- paste("",letters,sep="")
-        return (MyMatrix)
-}
-
-MMPC.Gsquared <- function(observedMatrix,target,selected,subset) {
-    # if(!length(subset)) subset <- sample(c(1:(target-1)),1)
-
-    # tmp <- c("1")
-    sum=0.0
-    partialMatrix <- observedMatrix[,c(target,selected,subset)]
-    # while(dim(partialMatrix)[1]>0) {
-    for(i in 1:10) {
-        rownameList <- rownames(observedMatrix[which(duplicated(observedMatrix[1,])),])
-        Sabc <- MMPC.Count(observedMatrix[,c(target,selected,subset)],observedMatrix[,c(target,selected,subset)][1,])
-        Sac <- MMPC.Count(observedMatrix[,c(target,subset)],observedMatrix[,c(target,subset)][1,])
-        Sbc <- MMPC.Count(observedMatrix[,c(selected,subset)],observedMatrix[,c(selected,subset)][1,])
-        if(length(subset)==1) Sc <- MMPC.Count(observedMatrix[,subset],observedMatrix[,subset][1],vec=TRUE)
-        else Sc <- MMPC.Count(observedMatrix[,subset],observedMatrix[,subset][1,])
-        # tmp <- c(tmp,Sabc[2:length(Sabc)])
-        print(Sabc)
-        observedMatrix <- MMPC.UpdateMatrix(observedMatrix,rownameList)
-        # print(dim(partialMatrix))
-        sum=sum+(2*Sabc*log((Sabc*Sc)/(Sac*Sbc)))
-        # print(dim(partialMatrix)[1])
-        print(sum)
+MMPC.MightyOfSet <- function(observedMatrix) {
+    n <- dim(observedMatrix)[2]
+    Df <- c()
+    for (i in 1:n) {
+        Df[i] <- length(unique(observedMatrix[,i]))
     }
-    return (sum)
+    return (Df)
 }
 
-MMPC.UpdateMatrix <- function(partialMatrix,toBeDeletedItems) {
-    # print(partialMatrix[!rownames(partialMatrix) %in% toBeDeletedItems[2:length(toBeDeletedItems)], ])
-    # print(toBeDeletedItems)
-    # print(dim(partialMatrix[!rownames(partialMatrix) %in% toBeDeletedItems,]))
-    return (partialMatrix[!rownames(partialMatrix) %in% toBeDeletedItems,])
+MMPC.DegreesOfFreedom <- function(Df,target,selected,subset) {
+    out <- (Df[target]-1)*(Df[selected]-1)
+    if(length(subset)==1) {
+        out <- out*Df[subset]
+    } else {
+        for (i in 1:length(subset)) {
+            out <- out*Df[subset[i]]
+        }
+    }
+    return (out)
 }
 
-MMPC.Count <- function(partialMatrix,target,vec=FALSE) {
-    # print(length(partialMatrix[which(duplicated(targetVector))])+1)
-    if(vec) tmp <- length(partialMatrix[which(duplicated(partialMatrix[1]))])+1
-    # if(vec) tmp <- c(length(partialMatrix[which(duplicated(targetVector))])+1,rownames(partialMatrix[which(duplicated(targetVector)),]))
-    else tmp <- dim(partialMatrix[which(duplicated(partialMatrix[1,])),])[1]+1
-    # else tmp <- c(dim(partialMatrix[which(duplicated(targetVector)),])[1]+1,rownames(partialMatrix[which(duplicated(targetVector)),]))
-    return (tmp)
+MMPC.MyFunction <- function(observedMatrix,target,selected,subset) {
+    out <- 0
+    ABCvec <- observedMatrix[1,c(target,selected,subset)]
+    ACvec <- ABCvec[c(1,3:length(ABCvec))]
+    BCvec <- ABCvec[2:length(ABCvec)]
+    Cvec <- ABCvec[3:length(ABCvec)]
+    tmp <- 1
+    partialMatrix <- observedMatrix[,c(target,selected,subset)]
+    while(tmp>0) {
+        ABClist <- Sabc(partialMatrix,ABCvec)
+        toBeDeletedItems <- ABClist[2:length(ABClist)]
+        ABCvalue <- ABClist[1]
+        ACvalue <- Sabc(observedMatrix[,c(target,subset)],ACvec)[1]
+        BCvalue <- Sabc(observedMatrix[,c(selected,subset)],BCvec)[1]
+        Cvalue <- Sabc(as.matrix(observedMatrix[,subset]),Cvec)[1]
+        partialMatrix <- partialMatrix[toBeDeletedItems,]
+        if(length(partialMatrix)==length(c(target,selected,subset))) tmp <- 1
+        else tmp <- dim(as.matrix(partialMatrix))[1]
+        if(tmp>0) {
+            if(length(partialMatrix)==length(c(target,selected,subset))) {
+                    ABCvec <- partialMatrix
+                    tmp <- 0
+                }
+            else ABCvec <- partialMatrix[1,]
+            ACvec <- ABCvec[c(1,3:length(ABCvec))]
+            BCvec <- ABCvec[2:length(ABCvec)]
+            Cvec <- ABCvec[3:length(ABCvec)]
+        }
+        out <- out + 2*ABCvalue*log((ABCvalue*Cvalue)/(ACvalue*BCvalue))
+    }
+    return (out)
 }
 
-tmp=MMPC.Example(100)
+MMPC.Pvalue <- function(a,b,c) {
+    tmp <- MMPC.Example(1000)
+    statistic <- MMPC.MyFunction(tmp,a,b,c)
+    df <- MMPC.MightyOfSet(tmp)
+    df <- MMPC.DegreesOfFreedom(df,a,b,c)
+    out <- pchisq(statistic, df, lower.tail = FALSE)
+    return (out)
+}
+
+
+# MMPC.ABCvalue <- function(partialMatrix,a,b,c) {
+#     partial_abc <- partialMatrix[,c(a,b,c)]
+#     if(dim(partialMatrix)[1]==1) vec_abc <- partial_abc
+#     else vec_abc <- partial_abc[1,]
+#     out <- Sabc(vec_abc,partial_abc)
+#     return (out)
+# }
+
+# MMPC.Svalue <- function(observedMatrix,a,b,c) {
+#     out <- c()
+#     partial_ac <- observedMatrix[,c(a,c)]
+#     partial_bc <- observedMatrix[,c(b,c)]
+#     if(length(c)==1) partial_c <- as.matrix(observedMatrix[,c(c)])
+#     else partial_c <- observedMatrix[,c(c)]
+#     vec_ac <- partial_ac[1,]
+#     vec_bc <- partial_bc[1,]
+#     if(length(c)==1) vec_c <- partial_c
+#     else vec_c <- partial_c[1,]
+#     out <- c(out, Sabc(vec_ac,partial_ac))
+#     out <- c(out, Sabc(vec_bc,partial_bc))
+#     out <- c(out, Sabc(vec_c,partial_c))
+#     return (out)
+# }
+
+# MMPC.Gvalue <- function(observedMatrix,target,selected,subset) {
+#     out <- 0
+#     tmp <- 1
+#     ABCval <- MMPC.ABCvalue(observedMatrix,target,selected,subset)
+#     toBeDeletedItems <- c()
+#     # while(tmp>0) {
+#     for(i in 1:10) {
+#         Svalues <- MMPC.Svalue(observedMatrix,target,selected,subset)
+#         out <- out + 2*ABCval[1]*log((ABCval[1]*Svalues[3])/(Svalues[1]*Svalues[2]))
+#         print(ABCval[2:length(ABCval)])
+#         toBeDeletedItems <- c(toBeDeletedItems,ABCval[2:length(ABCval)])
+#         tmp <- dim(observedMatrix[toBeDeletedItems,])[1]
+#         ABCval <- MMPC.ABCvalue(observedMatrix[toBeDeletedItems,],target,selected,subset)
+#     }
+#     return (out);
+# }
+
+
+# MMPC.Test <- function(rows) {
+#         n <- 1
+#         MyMatrix <- matrix(,rows,200)
+#         for(i in 1:200) {
+#                 for(j in 1:rows) {
+#                         MyMatrix[j,n] <- sample(c(0,1),1,replace=TRUE,prob=c(0.2,0.8))
+#                 }
+#                 n <- n + 1
+#         }
+#         rownames(MyMatrix) <- 1:rows
+#         # colnames(MyMatrix) <- paste("",letters,sep="")
+#         return (MyMatrix)
+# }
+
+# MMPC.Gsquared <- function(observedMatrix,target,selected,subset) {
+#     # if(!length(subset)) subset <- sample(c(1:(target-1)),1)
+
+#     # tmp <- c("1")
+#     sum=0.0
+#     partialMatrix <- observedMatrix[,c(target,selected,subset)]
+#     # while(dim(partialMatrix)[1]>0) {
+#     for(i in 1:10) {
+#         rownameList <- rownames(observedMatrix[which(duplicated(observedMatrix[1,])),])
+#         Sabc <- MMPC.Count(observedMatrix[,c(target,selected,subset)],observedMatrix[,c(target,selected,subset)][1,])
+#         Sac <- MMPC.Count(observedMatrix[,c(target,subset)],observedMatrix[,c(target,subset)][1,])
+#         Sbc <- MMPC.Count(observedMatrix[,c(selected,subset)],observedMatrix[,c(selected,subset)][1,])
+#         if(length(subset)==1) Sc <- MMPC.Count(observedMatrix[,subset],observedMatrix[,subset][1],vec=TRUE)
+#         else Sc <- MMPC.Count(observedMatrix[,subset],observedMatrix[,subset][1,])
+#         # tmp <- c(tmp,Sabc[2:length(Sabc)])
+#         print(Sabc)
+#         observedMatrix <- MMPC.UpdateMatrix(observedMatrix,rownameList)
+#         # print(dim(partialMatrix))
+#         sum=sum+(2*Sabc*log((Sabc*Sc)/(Sac*Sbc)))
+#         # print(dim(partialMatrix)[1])
+#         print(sum)
+#     }
+#     return (sum)
+# }
+
+# MMPC.UpdateMatrix <- function(partialMatrix,toBeDeletedItems) {
+#     # print(partialMatrix[!rownames(partialMatrix) %in% toBeDeletedItems[2:length(toBeDeletedItems)], ])
+#     # print(toBeDeletedItems)
+#     # print(dim(partialMatrix[!rownames(partialMatrix) %in% toBeDeletedItems,]))
+#     return (partialMatrix[!rownames(partialMatrix) %in% toBeDeletedItems,])
+# }
+
+# MMPC.Count <- function(partialMatrix,target,vec=FALSE) {
+#     # print(length(partialMatrix[which(duplicated(targetVector))])+1)
+#     if(vec) tmp <- length(partialMatrix[which(duplicated(partialMatrix[1]))])+1
+#     # if(vec) tmp <- c(length(partialMatrix[which(duplicated(targetVector))])+1,rownames(partialMatrix[which(duplicated(targetVector)),]))
+#     else tmp <- dim(partialMatrix[which(duplicated(partialMatrix[1,])),])[1]+1
+#     # else tmp <- c(dim(partialMatrix[which(duplicated(targetVector)),])[1]+1,rownames(partialMatrix[which(duplicated(targetVector)),]))
+#     return (tmp)
+# }
 
 
 
--- mmpc.cpp
+++ mmpc.cpp
@@ -1,27 +1,58 @@
 #include <Rcpp.h>
+#include <omp.h>
 using namespace Rcpp;
 
 // [[Rcpp::export]]
-void Gsquare(NumericVector a) {
-	for (int i = 0; i < a.size(); i++)
+NumericVector Sabc(NumericMatrix A,NumericVector x) {
+	int n=A.nrow(),m=A.ncol(),S_abc=0,S_abc_tmp=0;
+	NumericVector out;
+	out.push_back(0);
+	#pragma omp parallel
 	{
-		Rprintf("%f ",a[i]);
+		#pragma omp for
+		for (int i = 0; i < n; i++)
+		{
+			S_abc_tmp=0;
+			for (int j = 0; j < m; j++)
+			{
+				if(x[j]==A(i,j)) S_abc_tmp++;
+			}
+			if(S_abc_tmp==m) {
+				out[0]++;
+				out.push_back((-1)*(i+1));
+			}
+		}
 	}
-	Rprintf("\n");
+	return out;
 }
 
 // [[Rcpp::export]]
-int Sabc(NumericVector x, NumericMatrix A) {
-	int n=A.nrow(),m=A.ncol(),S_abc=0,S_abc_tmp=0;
-	for (int i = 0; i < n; i++)
-	{
-		S_abc_tmp=0;
-		for (int j = 0; j < m; j++)
-		{
-			if(x[j]==A(i,j)) S_abc_tmp++;
-		}
-		if(S_abc_tmp==m) S_abc++;
+void Gsquare(NumericVector x) {
+	if(2) {
+		Rprintf("Hi\n");
 	}
-	return S_abc;
-	// return x.size();
 }
+
+// double Gsquare(NumericVector x, NumericMatrix A) {
+// 	return 0.0;
+// }
+
+// NumericVector Sabc(NumericVector x, NumericMatrix A) {
+// 	int n=A.nrow(),m=A.ncol(),S_abc=0,S_abc_tmp=0;
+// 	NumericVector S;
+// 	S.push_back(0);
+// 	for (int i = 0; i < n; i++)
+// 	{
+// 		S_abc_tmp=0;
+// 		for (int j = 0; j < m; j++)
+// 		{
+// 			if(x[j]==A(i,j)) S_abc_tmp++;
+// 		}
+// 		if(S_abc_tmp==m) {
+// 			S[0]++;
+// 			S.push_back(i);
+// 		}
+// 	}
+// 	return S;
+// 	// return x.size();
+// }
