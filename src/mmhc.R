f486f36f79ae02cf59bef05fb43fc47c6b210374
Michael <michael@michael-V5-131.(none)>
2014-05-05 21:42:32 +0200

    Latest work on the project files.

--- src/mmhc.R
+++ src/mmhc.R
@@ -1,6 +1,8 @@
 if(!("Rcpp" %in% rownames(installed.packages()))) install.packages("Rcpp")
+if(!("sets" %in% rownames(installed.packages()))) install.packages("sets")
 
-library("Rcpp")
+require("Rcpp")
+require("sets")
 sourceCpp("mmhc.cpp");
 
 MMPC <- function() UseMethod("MMPC")
@@ -21,6 +23,29 @@ MMPC.Example <- function(rows,cols=5) {
                 if(observedMatrix[n,4]) observedMatrix[n,5] <- sample(c(0,1),1,replace=TRUE,prob=c(0.1,0.9))
                 if(observedMatrix[n,4]==2) observedMatrix[n,5] <- sample(c(0,1),1,replace=TRUE,prob=c(0.4,0.6))
                 if(observedMatrix[n,4]==3) observedMatrix[n,5] <- sample(c(0,1),1,replace=TRUE,prob=c(0.99,0.01))
+                n <- n+1
+        }
+        rownames(observedMatrix) <- 1:rows
+        colnames(observedMatrix) <- names
+        return (observedMatrix)
+}
+
+MMPC.Example2 <- function(rows,cols=5) {
+        names <- c("Difficulty","Intelligence","SAT","Grade","Letter")
+        n <- 1
+        observedMatrix <- matrix(,rows,cols)
+        observedMatrix[,1] <- sample(c("high","low"),rows,replace=TRUE,prob=c(0.6,0.4))
+        observedMatrix[,2] <- sample(c("high","low"),rows,replace=TRUE,prob=c(0.7,0.3))
+        for (i in observedMatrix[,2]) {
+                if(i=="low") observedMatrix[n,3] <- sample(c("bad","good"),1,replace=TRUE,prob=c(0.95,0.05))
+                if(i=="high") observedMatrix[n,3] <- sample(c("bad","good"),1,replace=TRUE,prob=c(0.2,0.8))
+                if(i=="high"&&observedMatrix[n,1]=="high") observedMatrix[n,4] <- sample(c("A","B","C"),1,replace=TRUE,prob=c(0.5,0.3,0.2))
+                if(i=="high"&&observedMatrix[n,1]=="low") observedMatrix[n,4] <- sample(c("A","B","C"),1,replace=TRUE,prob=c(0.9,0.08,0.02))
+                if(i=="low"&&observedMatrix[n,1]=="high") observedMatrix[n,4] <- sample(c("A","B","C"),1,replace=TRUE,prob=c(0.05,0.25,0.7))
+                if(i=="low"&&observedMatrix[n,1]=="low") observedMatrix[n,4] <- sample(c("A","B","C"),1,replace=TRUE,prob=c(0.3,0.4,0.3))
+                if(observedMatrix[n,4]=="A") observedMatrix[n,5] <- sample(c("bad","good"),1,replace=TRUE,prob=c(0.1,0.9))
+                if(observedMatrix[n,4]=="B") observedMatrix[n,5] <- sample(c("bad","good"),1,replace=TRUE,prob=c(0.4,0.6))
+                if(observedMatrix[n,4]=="C") observedMatrix[n,5] <- sample(c("bad","good"),1,replace=TRUE,prob=c(0.99,0.01))
                 n <- n+1
         }
         rownames(observedMatrix) <- 1:rows
@@ -57,13 +82,15 @@ MMPC.Statistic <- function(observedMatrix,target,selected,subset) {
     Cvec <- ABCvec[3:length(ABCvec)]
     tmp <- 1
     partialMatrix <- observedMatrix[,c(target,selected,subset)]
+    type <- 1
+    if(class(observedMatrix[1,1])=="character") type<-0
     while(tmp>0) {
-        ABClist <- Sabc(partialMatrix,ABCvec)
+        ABClist <- Test(partialMatrix,ABCvec,type)
         toBeDeletedItems <- ABClist[2:length(ABClist)]
         ABCvalue <- ABClist[1]
-        ACvalue <- Sabc(observedMatrix[,c(target,subset)],ACvec)[1]
-        BCvalue <- Sabc(observedMatrix[,c(selected,subset)],BCvec)[1]
-        Cvalue <- Sabc(as.matrix(observedMatrix[,subset]),Cvec)[1]
+        ACvalue <- Test(observedMatrix[,c(target,subset)],ACvec,type)[1]
+        BCvalue <- Test(observedMatrix[,c(selected,subset)],BCvec,type)[1]
+        Cvalue <- Test(as.matrix(observedMatrix[,subset]),Cvec,type)[1]
         partialMatrix <- partialMatrix[toBeDeletedItems,]
         if(length(partialMatrix)==length(c(target,selected,subset))) tmp <- 1
         else tmp <- dim(as.matrix(partialMatrix))[1]
@@ -79,15 +106,16 @@ MMPC.Statistic <- function(observedMatrix,target,selected,subset) {
         }
         out <- out + 2*ABCvalue*log((ABCvalue*Cvalue)/(ACvalue*BCvalue))
     }
+    TOL <- 0.001
+    if(out < TOL) out <- 0
     return (out)
 }
 
 MMPC.Pvalue <- function(a,b,c) {
-    tmp <- MMPC.Example(1000)
+    tmp <- MMPC.Example2(1000)
     statistic <- MMPC.Statistic(tmp,a,b,c)
     df <- MMPC.Cardinality(tmp)
     df <- Df(df,a,b,c)
-    # df <- MMPC.DegreesOfFreedom(df,a,b,c)
     out <- pchisq(statistic, df, lower.tail = FALSE)
     return (out)
 }
--- src/mmhc.cpp
+++ src/mmhc.cpp
@@ -1,5 +1,7 @@
 #include <Rcpp.h>
 #include <omp.h>
+#include <Rinternals.h>
+#include <R.h>
 using namespace Rcpp;
 
 // [[Rcpp::export]]
@@ -24,6 +26,88 @@ NumericVector Sabc(NumericMatrix A,NumericVector x) {
 		}
 	}
 	return out;
+}
+
+bool allCpp(NumericVector& x,NumericVector& y) {
+	bool out=TRUE;
+	if(x.size()!=y.size()) out=FALSE;
+	else {
+		for (int i = 0; i < x.size(); i++)
+			if(x[i]!=y[i]) {out=FALSE;break;}
+	}
+	return out;
+}
+
+bool allCp(CharacterVector& x,CharacterVector& y) {
+	bool out=TRUE;
+	if(x.size()!=y.size()) out=FALSE;
+	else {
+		for (int i = 0; i < x.size(); i++)
+			if(x[i]!=y[i]) {out=FALSE;break;}
+	}
+	return out;
+}
+
+// [[Rcpp::export]]
+bool allC(SEXP s,SEXP t) {
+	bool out=TRUE;
+	int *x,*y;
+	if(TYPEOF(s)==INTSXP) {x=INTEGER(s);y=INTEGER(t);}
+	// if(TYPEOF(s)==REALSXP) {x=REAL(s);y=REAL(t);}
+	// if(TYPEOF(s)==STRSXP) {x=CHAR(s);y=CHAR(t);}
+	if(XLENGTH(s)!=XLENGTH(t)) out=FALSE;
+	else {
+		for (int i = 0; i < XLENGTH(s); i++)
+			if(x[i]!=y[i]) {out=FALSE;break;}
+	}
+	return out;
+}
+
+// [[Rcpp::export]]
+NumericVector Test(SEXP a,SEXP X,int type=1) {
+	NumericVector out;
+	out.push_back(0);
+	if(type) {
+		NumericVector x(X);
+		NumericMatrix A(a);
+		NumericVector tmp(A.nrow());
+
+		int n=A.nrow(),m=A.ncol();
+		#pragma omp parallel
+		{
+			#pragma omp for
+			for (int i = 0; i < n; i++)
+			{
+				tmp=A.row(i);
+				if(allCpp(x,tmp)) {out[0]++; out.push_back((-1)*(i+1));}
+			}
+		}
+	}
+	else {
+		CharacterVector x(X);
+		CharacterMatrix A(a);
+		CharacterVector tmp(A.nrow());
+
+		int n=A.nrow(),m=A.ncol();
+		#pragma omp parallel
+		{
+			#pragma omp for
+			for (int i = 0; i < n; i++)
+			{
+				tmp=A.row(i);
+				if(allCp(x,tmp)) {out[0]++; out.push_back((-1)*(i+1));}
+			}
+		}
+	}
+	return out;
+}
+
+// [[Rcpp::export]]
+bool Proof(NumericVector x, NumericVector y) {
+	// NumericVector out(x);
+	// if(x==y) out=1;
+	// else out=0;
+	return is_true(any(x==y));
 }
 
 // [[Rcpp::export]]
