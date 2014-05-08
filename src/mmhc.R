5eb6b3eaa8d335defe98f334181d0e31a519662f
Michael <michael@michael-V5-131.(none)>
2014-05-08 15:09:01 +0200

    Made code a bit faster.

--- src/mmhc.R
+++ src/mmhc.R
@@ -4,22 +4,15 @@
 
 if (!("Rcpp" %in% rownames(installed.packages())))
   install.packages("Rcpp")
-if(!("sets" %in% rownames(installed.packages())))
-  install.packages("sets")
+# if(!("sets" %in% rownames(installed.packages())))
+#   install.packages("sets")
 
 require("Rcpp")
-require("sets")
+require("bnlearn")
+require("rbenchmark")
+# require("sets")
 sourceCpp("mmhc.cpp")
 source("mmhc_test.R")
-
-Cardinality <- function(observedMatrix) {
-    n <- dim(observedMatrix)[2]
-    Df <- c()
-    for (i in 1:n) {
-        Df[i] <- length(unique(observedMatrix[,i]))
-    }
-    return (Df)
-}
 
 Run <- function(mat) {
     cpc <- c()
@@ -32,27 +25,18 @@ Run <- function(mat) {
 
     cpc2 <- c()
 
-    while (target != 6) {
+    while (target != dim(mat)[2]) {
 
         for (i in iterate) {
             tmp <- mat[, c(target, i, cpc)]
-            df <- Cardinality(tmp)
-            pvalue <- Statistic(tmp, unique(tmp), df)
-            if (pvalue > 0.05) {
-                # bads <- c(bads, -i)
-            } else {
+            pvalue <- Statistic(tmp, unique(tmp))
+            if (pvalue < 0.05)
                 goods <- c(goods, i)
-                # values <- c(values, pvalue)
-            }
         }
-        # u <- cbind(goods, values)
-        # cpc <- c(cpc, which(max(u[,2]) == u)-dim(u)[1])
         cpc <- c(cpc, goods[1])
-        iterate <- goods[goods != cpc]
+        iterate <- goods[goods != goods[1]]
         goods <- c()
-        # bads <- c()
         maxP <- 0.0
-        # target <- target
         if(length(iterate)==0) {
             cpc2 <- c(cpc2, cpc, 0)
             cpc <- c()
@@ -61,33 +45,15 @@ Run <- function(mat) {
             iterate <- iterate[iterate != target]
         }
     }
-
-    # goods <- c()
-    # bads <- c()
-    # iterate <- sample(1:dim(mat)[2])
-    # target <- sample(iterate,1)
-    # iterate <- 1:dim(mat)[2]
-    # nots <- c(cpc, target)
-    # iterate <- iterate[iterate != nots]
-    # cpc <- cpc[cpc != target]
-    # powSet <- 2 ^ cpc
-    # powSet <- as.vector(powSet)
-
-    # for (i in iterate) {
-    #     tmp <- mat[, c(target, i, cpc)]
-    #     df <- Cardinality(tmp)
-    #     pvalue <- Statistic(tmp, unique(tmp), df)
-
-    #     if (pvalue < 0.05)
-    #         bads <- c(bads, i)
-    #     else {
-    #         goods <- c(goods, pvalue)
-    #     }
-    #     cpc <- c(cpc, pvalue)
-    # }
-
-    # mat <- as.matrix(Example(100))[, c(target, selected, subset)]
-    # df <- Cardinality(mat)
-    # out <- Statistic(mat, unique(mat), df)
     return (cpc2)
 }
+
+tempo <- as.matrix(Example(100, char=FALSE))
+
+bench <- function(tmp) {
+    Statistic(tmp, unique(tmp))
+}
+
+a<-benchmark(Run(tempo),replications=1)
+x<-benchmark(bench(tempo),replications=29)
+y<-benchmark(Cardinality(tempo),replications=1000)
--- src/mmhc.cpp
+++ src/mmhc.cpp
@@ -4,165 +4,253 @@
 #include <R.h>
 using namespace Rcpp;
 
+// bool allC(SEXP a, SEXP b, int del = -1) {
+// 	bool out = TRUE;
+// 	switch (TYPEOF(a)) {
+// 		case INTSXP:
+// 		{
+// 			IntegerVector x(a), y(b);
+// 			if(x.size()!=y.size()) out=FALSE;
+// 			else {
+// 				for (int i = 0; i < x.size(); i++) {
+// 					if(i == del) continue;
+// 					if(del == -2 && (i == 0 || i == 1)) continue;
+// 					if(x[i]!=y[i]) {out=FALSE;break;}
+// 				}
+// 			}
+// 			break;
+// 		}
+// 		case REALSXP:
+// 		{
+// 			NumericVector x(a), y(b);
+// 			if(x.size()!=y.size()) out=FALSE;
+// 			else {
+// 				for (int i = 0; i < x.size(); i++) {
+// 					if(i == del) continue;
+// 					if(del == -2 && (i == 0 || i == 1)) continue;
+// 					if(x[i]!=y[i]) {out=FALSE;break;}
+// 				}
+// 			}
+// 			break;
+// 		}
+// 		default:
+// 		{
+// 			CharacterVector x(a), y(b);
+// 			if(x.size()!=y.size()) out=FALSE;
+// 			else {
+// 				for (int i = 0; i < x.size(); i++) {
+// 					if(i == del) continue;
+// 					if(del == -2 && (i == 0 || i == 1)) continue;
+// 					if(x[i]!=y[i]) {out=FALSE;break;}
+// 				}
+// 			}
+// 			break;
+// 		}
+// 	}
+// 	return out;
+// }
+
 bool allC(SEXP a, SEXP b, int del = -1) {
 	bool out = TRUE;
-	switch (TYPEOF(a)) {
-		case INTSXP:
-		{
-			IntegerVector x(a), y(b);
-			if(x.size()!=y.size()) out=FALSE;
-			else {
-				for (int i = 0; i < x.size(); i++) {
-					if(i == del) continue;
-					if(del == -2 && (i == 0 || i == 1)) continue;
-					if(x[i]!=y[i]) {out=FALSE;break;}
-				}
-			}
-			break;
-		}
-		case REALSXP:
-		{
-			NumericVector x(a), y(b);
-			if(x.size()!=y.size()) out=FALSE;
-			else {
-				for (int i = 0; i < x.size(); i++) {
-					if(i == del) continue;
-					if(del == -2 && (i == 0 || i == 1)) continue;
-					if(x[i]!=y[i]) {out=FALSE;break;}
-				}
-			}
-			break;
-		}
-		default:
-		{
-			CharacterVector x(a), y(b);
-			if(x.size()!=y.size()) out=FALSE;
-			else {
-				for (int i = 0; i < x.size(); i++) {
-					if(i == del) continue;
-					if(del == -2 && (i == 0 || i == 1)) continue;
-					if(x[i]!=y[i]) {out=FALSE;break;}
-				}
-			}
-			break;
+	NumericVector x(a), y(b);
+	if(x.size()!=y.size()) out=FALSE;
+	else {
+		for (int i = 0; i < x.size(); i++) {
+			if(i == del) continue;
+			if(del == -2 && (i == 0 || i == 1)) continue;
+			if(x[i]!=y[i]) {out=FALSE;break;}
 		}
 	}
 	return out;
 }
 
 // [[Rcpp::export]]
-double Statistic(SEXP x, SEXP y, SEXP z) {
+NumericVector Cardinality(SEXP x) {
+	NumericMatrix A(x);
+	NumericVector out;
+	int m = A.nrow(), n = A.ncol();
+
+	for (int i = 0; i < n; i++)
+	{
+		NumericVector count;
+		for (int j = 0; j < m; j++)
+		{
+			if(i == m)
+				count.push_back(A(i,j));
+			for (int k = 0; k < count.size(); k++)
+			{
+				if(A(i,j) != count[k])
+					count.push_back(A(i,j));
+			}
+		}
+		out.push_back(count.size());
+	}
+	return out;
+}
+
+// // [[Rcpp::export]]
+// double Statistic(SEXP x, SEXP y, SEXP z) {
+// 	int n, m;
+// 	NumericVector count(4,0.0);
+// 	NumericVector sum(1,0.0), pvalue(1,0.0);
+// 	NumericVector df(z);
+
+// 	switch(TYPEOF(x)) {
+// 		case INTSXP:
+// 		{
+// 			IntegerMatrix A(x);
+// 			IntegerMatrix B(y);
+// 			n = A.nrow();
+// 			m = B.nrow();
+// 			IntegerVector u;
+// 			IntegerVector v;
+
+// 			#pragma omp parallel
+// 			{
+// 				#pragma omp for 
+// 				for (int i = 0; i < m; i++)
+// 				{
+// 					count[0] = count[1] = count[2] = count[3] = 0;
+// 					u = B.row(i);
+// 					for (int j = 0; j < n; j++)
+// 					{
+// 						v = A.row(j);
+// 						if(allC(v,u))
+// 							count[0]++;
+// 						if(allC(v,u,1))
+// 							count[1]++;
+// 						if(allC(v,u,0))
+// 							count[2]++;
+// 						if(v.size() == n) 
+// 							count[3] = n;
+// 						else {
+// 							if(allC(v,u,-2))
+// 								count[3]++;
+// 						}
+// 					}
+// 					sum[0] += 2 * count[0] * log( ( count[0] * count[3] ) / ( count[1] * count[2] ) );
+// 				}
+// 			}
+// 			break;
+// 		}
+// 		case REALSXP:
+// 		{
+// 			NumericMatrix A(x);
+// 			NumericMatrix B(y);
+// 			n = A.nrow();
+// 			m = B.nrow();
+// 			NumericVector u;
+// 			NumericVector v;
+
+// 			#pragma omp parallel
+// 			{
+// 				#pragma omp for 
+// 				for (int i = 0; i < m; i++)
+// 				{
+// 					count[0] = count[1] = count[2] = count[3] = 0;
+// 					u = B.row(i);
+// 					for (int j = 0; j < n; j++)
+// 					{
+// 						v = A.row(j);
+// 						if(allC(v,u))
+// 							count[0]++;
+// 						if(allC(v,u,1))
+// 							count[1]++;
+// 						if(allC(v,u,0))
+// 							count[2]++;
+// 						if(allC(v,u,-2))
+// 							count[3]++;
+// 					}
+// 					sum[0] += 2 * count[0] * log( ( count[0] * count[3] ) / ( count[1] * count[2] ) );
+// 				}
+// 			}
+// 			break;
+// 		}
+// 		default:
+// 		{
+// 			CharacterMatrix A(x);
+// 			CharacterMatrix B(y);
+// 			n = A.nrow();
+// 			m = B.nrow();
+// 			CharacterVector u;
+// 			CharacterVector v;
+
+// 			#pragma omp parallel
+// 			{
+// 				#pragma omp for 
+// 				for (int i = 0; i < m; i++)
+// 				{
+// 					count[0] = count[1] = count[2] = count[3] = 0;
+// 					u = B.row(i);
+// 					for (int j = 0; j < n; j++)
+// 					{
+// 						v = A.row(j);
+// 						if(allC(v,u))
+// 							count[0]++;
+// 						if(allC(v,u,1))
+// 							count[1]++;
+// 						if(allC(v,u,0))
+// 							count[2]++;
+// 						if(allC(v,u,-2))
+// 							count[3]++;
+// 					}
+// 					sum[0] += 2 * count[0] * log( ( count[0] * count[3] ) / ( count[1] * count[2] ) );
+// 				}
+// 			}
+// 			break;
+// 		}
+// 	}
+
+// 	int DF = (df[0] - 1) * (df[1] - 1);
+
+// 	for (int i = 2; i < df.size(); i++)
+// 	{
+// 		DF *= df[i];
+// 	}
+
+// 	pvalue = pchisq(sum, DF, FALSE);
+// 	return pvalue[0];
+// }
+
+// [[Rcpp::export]]
+double Statistic(SEXP x, SEXP y) {
 	int n, m;
 	NumericVector count(4,0.0);
 	NumericVector sum(1,0.0), pvalue(1,0.0);
-	NumericVector df(z);
+	NumericVector df;
+	
+	NumericMatrix A(x);
+	NumericMatrix B(y);
+	n = A.nrow();
+	m = B.nrow();
+	NumericVector u;
+	NumericVector v;
 
-	switch(TYPEOF(x)) {
-		case INTSXP:
+	#pragma omp parallel
+	{
+		#pragma omp for 
+		for (int i = 0; i < m; i++)
 		{
-			IntegerMatrix A(x);
-			IntegerMatrix B(y);
-			n = A.nrow();
-			m = B.nrow();
-			IntegerVector u;
-			IntegerVector v;
-
-			#pragma omp parallel
+			count[0] = count[1] = count[2] = count[3] = 0;
+			u = B.row(i);
+			for (int j = 0; j < n; j++)
 			{
-				#pragma omp for 
-				for (int i = 0; i < m; i++)
-				{
-					count[0] = count[1] = count[2] = count[3] = 0;
-					u = B.row(i);
-					for (int j = 0; j < n; j++)
-					{
-						v = A.row(j);
-						if(allC(v,u))
-							count[0]++;
-						if(allC(v,u,1))
-							count[1]++;
-						if(allC(v,u,0))
-							count[2]++;
-						if(v.size() == n) 
-							count[3] = n;
-						else {
-							if(allC(v,u,-2))
-								count[3]++;
-						}
-					}
-					sum[0] += 2 * count[0] * log( ( count[0] * count[3] ) / ( count[1] * count[2] ) );
-				}
+				v = A.row(j);
+				if(allC(v,u))
+					count[0]++;
+				if(allC(v,u,1))
+					count[1]++;
+				if(allC(v,u,0))
+					count[2]++;
+				if(allC(v,u,-2))
+					count[3]++;
 			}
-			break;
-		}
-		case REALSXP:
-		{
-			NumericMatrix A(x);
-			NumericMatrix B(y);
-			n = A.nrow();
-			m = B.nrow();
-			NumericVector u;
-			NumericVector v;
-
-			#pragma omp parallel
-			{
-				#pragma omp for 
-				for (int i = 0; i < m; i++)
-				{
-					count[0] = count[1] = count[2] = count[3] = 0;
-					u = B.row(i);
-					for (int j = 0; j < n; j++)
-					{
-						v = A.row(j);
-						if(allC(v,u))
-							count[0]++;
-						if(allC(v,u,1))
-							count[1]++;
-						if(allC(v,u,0))
-							count[2]++;
-						if(allC(v,u,-2))
-							count[3]++;
-					}
-					sum[0] += 2 * count[0] * log( ( count[0] * count[3] ) / ( count[1] * count[2] ) );
-				}
-			}
-			break;
-		}
-		default:
-		{
-			CharacterMatrix A(x);
-			CharacterMatrix B(y);
-			n = A.nrow();
-			m = B.nrow();
-			CharacterVector u;
-			CharacterVector v;
-
-			#pragma omp parallel
-			{
-				#pragma omp for 
-				for (int i = 0; i < m; i++)
-				{
-					count[0] = count[1] = count[2] = count[3] = 0;
-					u = B.row(i);
-					for (int j = 0; j < n; j++)
-					{
-						v = A.row(j);
-						if(allC(v,u))
-							count[0]++;
-						if(allC(v,u,1))
-							count[1]++;
-						if(allC(v,u,0))
-							count[2]++;
-						if(allC(v,u,-2))
-							count[3]++;
-					}
-					sum[0] += 2 * count[0] * log( ( count[0] * count[3] ) / ( count[1] * count[2] ) );
-				}
-			}
-			break;
+			sum[0] += 2 * count[0] * log( ( count[0] * count[3] ) / ( count[1] * count[2] ) );
 		}
 	}
 
+	df = Cardinality(A);
+	
 	int DF = (df[0] - 1) * (df[1] - 1);
 
 	for (int i = 2; i < df.size(); i++)
