fbe3da5635f810919f5cb95927683f12e4b40d96
Michael <michael@michael-V5-131.(none)>
2014-05-11 17:33:14 +0200

    Nothing is working...

--- src/mmhc.R
+++ src/mmhc.R
@@ -14,59 +14,11 @@ require("rbenchmark")
 sourceCpp("mmhc.cpp")
 source("mmhc_test.R")
 
-# AddElement <- function(T, X, S, iterate) {
-#     repeat {
-#         if (length(iterate) == 0) {
-#             break
-#         }
-#         statisticMatrix <- mat[, c(T, X, S)]
-#         pvalue <- Statistic(statisticMatrix, unique(statisticMatrix))
-#         if (pvalue < alpha) {
-#             do
-#         } else {
-#             do too
-#         }
-#     }
-# }
-
-# recursion <- function(mat, T, X, S = NULL) {
-#     Y <- X
-#     out <- c()
-#     rejected <- c(0, 0)
-#     if (length(X) == 0) {
-#         return (S)
-#     } else {
-#         repeat {
-#             if (length(Y) == 0) {
-#                 out <- c(out, recursion(mat, T, X, rejected[1]))
-#                 break
-#             }
-#             statisticMatrix <- mat[, c(T, Y[1], S)]
-#             pvalue <- Statistic(statisticMatrix, unique(statisticMatrix))
-#             if (pvalue < 0.05) {
-#                 if (pvalue > rejected[2]) {
-#                     rejected <- c(Y[1], pvalue)
-#                 }
-#                 print(rejected)
-#                 Y <- Y[-1]
-#             } else {
-#                 X <- X[!(X == Y[1])]
-#                 Y <- Y[-1]
-#             }
-#         }
-#         return (out)
-#     }
-# }
-
-
-# ForwardPhase <- function(mat, dimMat = dim(mat)[2], alpha = 0.05) {
-
-# }
-
 MaxMinHeuristic <- function(T, CPC, mat, card, iterate) {
     reject <- c(0, 0)
     accepted <- c()
     for (X in iterate) {
+        print(c(X, T, CPC))
         statisticMatrix <- mat[, c(X, T, CPC)]
         pvalue <- Statistic(statisticMatrix, unique(statisticMatrix), card[c(X, T, CPC)])
         if (pvalue < 0.05) {
@@ -89,6 +41,7 @@ MMPCtmp <- function(mat, card, T) {
     tmp <- c()
 
     repeat {
+        tmp <- c()
         Fset <- MaxMinHeuristic(T, CPC[length(CPC)], mat, card, iterate)
         iterate <- iterate[!(iterate == Fset[1])]
         if (length(Fset) > 2) {
@@ -106,116 +59,6 @@ MMPCtmp <- function(mat, card, T) {
 }
 
 ForwardPhase <- function(mat, card, dimMat = dim(mat)[2], alpha = 0.05) {
-    # cpc <- matrix(0, dimMat, (dimMat-1))
-    # rejected <- c(0, 0)
-    # iterate <- c()
-    # accepted <- c()
-    # S <- 0
-    
-    # for (T in 1:dimMat) {
-    #     i <- 1
-    #     iterate <- 1:dimMat
-    #     iterate <- iterate[!(iterate == T)]
-    #     repeat {
-    #         if (rejected[1] == 0) {
-    #             S <- NULL
-    #         } else {
-    #             S <- rejected[1]
-    #             cpc[T, i] <- S
-    #             i <- i + 1
-    #         }
-    #             # print(S)
-    #         rejected <- c(0, 0)
-    #         if (length(accepted) != 0) {
-    #             iterate <- iterate[!(iterate == accepted)]
-    #         }
-    #         accepted <- c()
-    #         if (length(iterate) == 0) {
-    #             break
-    #         }
-    #         repeat {
-    #             if (length(iterate) == 0) {
-    #                 break
-    #             }
-    #             X <- iterate[1]
-    #             statisticMatrix <- mat[, c(T, X, S)]
-    #             pvalue <- Statistic(statisticMatrix, unique(statisticMatrix))
-    #             if (pvalue < alpha) {
-    #                 if (pvalue > rejected[2]) {
-    #                     rejected <- c(X, pvalue)
-    #                 }
-    #             } else {
-    #                 accepted <- c(accepted, X)
-    #             }
-    #             iterate <- iterate[!(iterate == X)]
-    #         }
-    #     }
-    # }
-
-    # for (T in 1:dim(mat)[2]) {
-    #     i <- 1
-    #     iterate <- 1:dim(mat)[2]
-    #     iterate <- iterate[!(iterate == T)]
-    #     repeat {
-    #         print(iterate)
-    #         if (length(iterate) == 0) {
-    #             break
-    #         }
-    #         accepted <- c()
-    #         rejected <- c(0, 0)
-    #         if (i == 1) {
-    #             S <- NULL
-    #         } else {
-    #             S <- cpc[T, (i-1)]
-    #         }
-    #         statisticMatrix <- mat[, c(T, iterate[1], S)]
-    #         pvalue <- Statistic(statisticMatrix, unique(statisticMatrix))
-    #         if (pvalue < alpha) {
-    #             # print(c("T: ", T, " X: ", iterate[1], " S: ", S))
-    #             cpc[T, i] <- iterate[1]
-    #             i <- i + 1
-    #             # if (pvalue > rejected[2]) {
-    #             #     rejected <- c(, pvalue)
-    #             # }
-    #         } else {
-    #             accepted <- c(accepted, iterate[1])
-    #         }
-    #         iterate <- iterate[!(iterate == accepted)]
-    #     }
-    # }
-
-    # cpc <- matrix(0, dimMat, dimMat)
-    # rejected <- c(0, 0)
-    # target <- 1:dimMat
-    # iterate <- target
-    # S <- 0
-
-    # for (T in target) {
-    #     for (i in 1:dimMat) {
-    #         rejected <- c(0, 0)
-    #         for (X in iterate[-T]) {
-    #             if (i == 1) {
-    #                 S <- NULL
-    #             } else {
-    #                 S <- cpc[T, i-1]
-    #                 if (S == 0) {
-    #                     break
-    #                 }
-    #             }
-    #             # print(c(T, X, S))
-    #             statisticMatrix <- mat[, c(T, X, S)]
-    #             pvalue <- Statistic(statisticMatrix, unique(statisticMatrix))
-    #             if (pvalue < 0.05) {
-    #                 if (pvalue > rejected[2]) {
-    #                     rejected <- c(X, pvalue)
-    #                 }
-    #             }
-    #             # print(c(T, X, S, X, pvalue))
-    #         }
-    #         cpc[T, i] <- rejected[1]
-    #     }
-    # }
-
     cpc <- c()
     target <- 1
     iterate <- 2:dim(mat)[2]
@@ -308,7 +151,9 @@ BackwardPhase <- function(mat, cpc, card) {
 
 tempo <- as.matrix(Example(1000, char=FALSE))
 card <- c(2,2,2,3,2)
-x <- MMPCtmp(tempo, card, 1)
+for (i in 1:5) {
+    print(MMPCtmp(tempo, card, i))
+}
 # FWP <- ForwardPhase(tempo, card)
 # BWP <- BackwardPhase(tempo, FWP, card)
 # test <- recursion(tempo, 1, 2:5)
