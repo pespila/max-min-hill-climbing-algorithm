70dcc6c4838adfb4e9eae72f900243b09ccf68e0
Michael <michael.bauer.tsv@gmail.com>
2014-09-06 18:02:02 +0200

    Working on report

---
+++ LaTex/talks/2nd talk/plot.R
@@ -0,0 +1,14 @@
+min <- 20
+max <- 30
+x<-seq(0,max,by=.25)
+y<-dchisq(x,df=10)
+plot(x,y,type="l", xlab = "G²", ylab = "f(G²) = X²")
+for (i in 1:4) {
+	xq=c(x[x>=min],max,min)
+	yq=c(y[x>=min],0,0)
+	polygon(xq,yq,col="orange")
+	file <- paste('chi',i,'.eps',sep="")
+	dev.copy(postscript,file)
+	dev.off()
+	min <- min - 5
+}
